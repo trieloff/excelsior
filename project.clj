0bdef05618c1870249b80fb453887affbeff2514
Lars Trieloff <lars@trieloff.net>
2015-08-23 18:47:06 +0200

    Initial project setup

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject excelsior "0.1.0-SNAPSHOT"
+  :description "Turns Excel into HTTP"
+  :url ""
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                  [dk.ative/docjure "1.8.0"]])

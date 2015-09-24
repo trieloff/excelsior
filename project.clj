177324b64fa632a8c0962d530d7d41cf896e1a8b
Lars Trieloff <lars@trieloff.net>
2015-09-24 19:49:32 +0200

    Project setup with compojure-api

--- project.clj
+++ project.clj
@@ -1,5 +1,11 @@
 (defproject excelsior "0.1.0-SNAPSHOT"
   :description "Turns Excel into HTTP"
-  :url ""
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                  [dk.ative/docjure "1.8.0"]])
+  :url "https://github.com/trieloff/excelsior"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [clj-time "0.9.0"] ; required due to bug in lein-ring
+                 [metosin/compojure-api "0.22.0"]
+                 [dk.active./docjure]]
+  :ring {:handler excelsior.handler/app}
+  :uberjar-name "server.jar"
+  :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]]
+                   :plugins [[lein-ring "0.9.6"]]}})

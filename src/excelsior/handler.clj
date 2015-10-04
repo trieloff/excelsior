b059e0d0d99557469fc9744b5f69d99ea65f1866
Lars Trieloff <lars@trieloff.net>
2015-10-04 21:40:45 +0200

    Basic API structure

--- src/excelsior/handler.clj
+++ src/excelsior/handler.clj
@@ -4,23 +4,24 @@
             [schema.core :as s]))
 
 (s/defschema Message {:message String})
-(s/defschema Calculation {:input String
+(s/defschema Spreadsheet {:input {s/Keyword (s/either Long String)}
+                          :meta {:customer String :spreadsheet String}
                           :output {s/Keyword (s/either Long String)}})
 
 (defapi app
   (swagger-ui)
   (swagger-docs
     {:info {:title "Excelsior"
-            :description "Compojure Api example"}
+            :description "A REST API for Spreadsheets"}
      :tags [{:name "hello", :description "says hello in Finnish"}
             {:name "formula" :description "evaluates Excel spreadsheet formulas"}]})
   (context* "/formula" []
             :tags ["formula"]
-            (GET* "/" []
-                  :return Calculation
-                  :query-params [input :- String]
+            (GET* "/:customer/:spreadsheet" request
+                  :return Spreadsheet
+                  :path-params [customer :- String spreadsheet :- String]
                   :summary "calculate the response value"
-                  (ok {:input input :output {(keyword input) 1.0}})))
+                  (ok {:input (:params request) :meta {:customer customer :spreadsheet spreadsheet} :output {:request (str (:params request))}})))
   (context* "/hello" []
     :tags ["hello"]
     (GET* "/" []

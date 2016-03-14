9861f15174bad60cc03ca4a4ad58578bdea85c76
Lars Trieloff <lars@trieloff.net>
2016-03-14 20:06:06 +0100

    Update lein-ring version
    No explicit dependency to clj-time needed

--- project.clj
+++ project.clj
@@ -2,11 +2,10 @@
   :description "Turns Excel into HTTP"
   :url "https://github.com/trieloff/excelsior"
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [clj-time "0.9.0"] ; required due to bug in lein-ring
                  [metosin/compojure-api "0.23.1"]
                  [com.taoensso/faraday "1.8.0"]
                  [dk.ative/docjure "1.11.0-SNAPSHOT"]]
   :ring {:handler excelsior.handler/app}
   :uberjar-name "server.jar"
   :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]]
-                   :plugins [[lein-ring "0.9.6"]]}})
+                   :plugins [[lein-ring "0.9.7"]]}})

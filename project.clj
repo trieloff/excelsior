a06b699bfd4292703911a8d6154007686ccc5678
Lars Trieloff <lars@trieloff.net>
2016-03-16 14:29:06 +0100

    Added environ dependencies

--- project.clj
+++ project.clj
@@ -4,10 +4,12 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [metosin/compojure-api "1.0.1"]
                  [com.taoensso/faraday "1.8.0"]
+                 [environ "1.0.2"]
                  [dk.ative/docjure "1.11.0-SNAPSHOT"]]
   :ring {:handler excelsior.handler/app}
   :uberjar-name "server.jar"
   :plugins [[test2junit "1.1.2"]
+            [lein-environ "1.0.2"]
             [lein-maven-s3-wagon "0.2.4"]]
   :deploy-repositories {"private" {:url "s3://leinrepo/releases/"
                                    :username :env/aws_access_key ;; gets environment variable AWS_ACCESS_KEY

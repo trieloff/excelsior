3c30dd8d713cdb46eebad043b3ac0c841a78239b
Lars Trieloff <lars@trieloff.net>
2016-03-17 10:50:50 +0100

    Moved test code out of main handler

--- src/excelsior/handler.clj
+++ src/excelsior/handler.clj
@@ -49,63 +49,6 @@
                    :block? true ; Block thread during table creation
                    })
 
-(far/describe-table client-opts :spreadsheets)
-
-
-(far/list-tables client-opts)
-
-(far/put-item client-opts
-              :customers
-              {:customer "hans"
-               :name (far/freeze "Hans Wurst" crypt-opts)})
-
-(far/put-item client-opts
-              :spreadsheets
-              {:customer "hans"
-               :spreadsheet "help"
-               :name "example.xls"
-               :url "/Users/ltrieloff/Documents/excelsior/resources/helloworld.xlsx"
-               :type "local"
-               :inputs (far/freeze #{"A1", "B1"} crypt-opts)
-               :output (far/freeze #{"C1" "C2"} crypt-opts)})
-
-(far/put-item client-opts
-              :spreadsheets
-              {:customer "hans"
-               :spreadsheet "nase"
-               :name "example.xls"
-               :url "/Users/ltrieloff/Documents/excelsior/resources/helloworld.xlsx"
-               :type "local"
-               :inputs (far/freeze #{"A1", "B1"} crypt-opts)
-               :output (far/freeze #{"D1" "D2"} crypt-opts)})
-
-(far/put-item client-opts
-              :spreadsheets
-              {:customer "hans"
-               :spreadsheet "nase-1"
-               :name "example.xls"
-               :url "/Users/ltrieloff/Documents/excelsior/resources/helloworld.xlsx"
-               :type "local"
-               :inputs (far/freeze #{"A1", "B1"} crypt-opts)
-               :output (far/freeze #{"D1" "D2"} crypt-opts)})
-
-(far/put-item client-opts
-              :spreadsheets
-              {:customer "hans"
-               :spreadsheet "nase-ring"
-               :name "example.xls"
-               :url "/Users/ltrieloff/Documents/excelsior/resources/helloworld.xlsx"
-               :type "local"
-               :inputs (far/freeze #{"A1", "B1"} crypt-opts)
-               :output (far/freeze #{"D1" "D2"} crypt-opts)})
-
-(far/get-item client-opts :customers {:customer "hans"})
-
-
-(count (far/with-thaw-opts crypt-opts (far/query client-opts
-                                          :spreadsheets {:customer [:eq "hans"]
-                                                         :spreadsheet [:begins-with "nase"]})))
-
 (defn is-unique-name? [customer name]
   (nil? (far/with-thaw-opts crypt-opts (far/get-item client-opts
                                              :spreadsheets
@@ -121,20 +64,6 @@
     name
     (str name "-" (count-variants customer name))))
 
-(is-unique-name? "hans" "wurst")
-(is-unique-name? "hans" "nase")
-(count-variants "hans" "nase")
-(make-unique-name "hans" "wurst")
-(make-unique-name "hans" "nase")
-
-(def formula-from-sheet
-  (doc/cell-fn "C1"
-               (first (doc/sheet-seq (doc/load-workbook "/Users/ltrieloff/Documents/excelsior/resources/helloworld.xlsx")))
-               "A1"))
-
-(def inputs #{"A2" "A1"})
-(def outputs #{"C2" "C1"})
-(def params {:A2 12 :A1 "Katze"})
 
 (defn sheet-formulas [inputs outputs sheet]
   (into (hash-map) (map #(conj

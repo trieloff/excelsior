fc8ad8ac727f2f2b9d4339df009ef479acc39f80
Lars Trieloff <lars@trieloff.net>
2016-05-17 14:29:25 +0200

    Switch from S3 Wagon deployment to lein plugin
    The S3 wagon has been flakey, but the lein-lambda plugin seems to work just fine.

--- project.clj
+++ project.clj
@@ -14,7 +14,13 @@
                    ".lein-env"] ; let's see if we can sneak the environment variables into the binary
   :plugins [[test2junit "1.1.2"]
             [lein-environ "1.0.2"]
-            [lein-maven-s3-wagon "0.2.5"]]
+            [lein-aws-api-gateway "1.10.68-1"]
+            [lein-clj-lambda "0.4.0"]
+            ;[lein-maven-s3-wagon "0.2.5"]
+            ]
+  :api-gateway {:api-id "arzz6rda99"
+                ;;:deploy "dev"
+                :swagger "target/swagger.json"}
   :test2junit-output-dir ~(or (System/getenv "CIRCLE_TEST_REPORTS") "target/test2junit")
   :env {:aws-access-key #=(eval (System/getenv "AWS_ACCESS_KEY"))
         :aws-secret-key #=(eval (System/getenv "AWS_SECRET_KEY"))
@@ -23,6 +29,21 @@
   :deploy-repositories {"private" {:url "s3://leinrepo/releases/"
                                    :username :env/aws_access_key ;; gets environment variable AWS_ACCESS_KEY
                                    :password :env/aws_secret_key}}
+  :lambda {"dev" [{:handler "excelsior.handler.Lambda"
+                  :memory-size 512
+                  :timeout 300
+                  :function-name "excelsior-dev"
+                  :region "us-east-1"
+                  :s3 {:bucket "leinrepo"
+                       :object-key "excelsior-dev.jar"}}]
+         "production" [{:handler "excelsior.handler.Lambda"
+                        :memory-size 512
+                        :timeout 300
+                        :function-name "excelsior-prod"
+                        :region "us-east-1"
+                        :s3 {:bucket "leinrepo"
+                            :object-key "excelsior-release.jar"}}]}
+
   :repositories {"private" {:url "s3://leinrepo/releases/"
                             :username :env/aws_access_key ;; gets environment variable AWS_ACCESS_KEY
                             :password :env/aws_secret_key}}

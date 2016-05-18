bb02c081ac1ab2947cabfb7f79c6b2d21a7506af
Lars Trieloff <lars@trieloff.net>
2016-05-18 11:37:19 +0200

    More dead code that needs to go

--- test/excelsior/handler_test.clj
+++ test/excelsior/handler_test.clj
@@ -1,73 +1,9 @@
 (ns excelsior.handler-test
   (:require [clojure.test :refer :all]
-            [taoensso.faraday :as far]
             [dk.ative.docjure.spreadsheet :as doc]
             [excelsior.handler :refer :all]
             [cheshire.core :refer :all]
             [clojure.walk :refer :all]))
-
-(far/describe-table client-opts :spreadsheets)
-
-
-(far/list-tables client-opts)
-
-(far/put-item client-opts
-              :customers
-              {:customer "hans"
-               :name (far/freeze "Hans Wurst" crypt-opts)})
-
-(far/put-item client-opts
-              :spreadsheets
-              {:customer "hans"
-               :spreadsheet "help"
-               :name "example.xls"
-               :url "/Users/ltrieloff/Documents/excelsior/resources/helloworld.xlsx"
-               :type "local"
-               :inputs (far/freeze #{"A1", "B1"} crypt-opts)
-               :output (far/freeze #{"C1" "C2"} crypt-opts)})
-
-(far/put-item client-opts
-              :spreadsheets
-              {:customer "hans"
-               :spreadsheet "nase"
-               :name "example.xls"
-               :url "/Users/ltrieloff/Documents/excelsior/resources/helloworld.xlsx"
-               :type "local"
-               :inputs (far/freeze #{"A1", "B1"} crypt-opts)
-               :output (far/freeze #{"D1" "D2"} crypt-opts)})
-
-(far/put-item client-opts
-              :spreadsheets
-              {:customer "hans"
-               :spreadsheet "nase-1"
-               :name "example.xls"
-               :url "/Users/ltrieloff/Documents/excelsior/resources/helloworld.xlsx"
-               :type "local"
-               :inputs (far/freeze #{"A1", "B1"} crypt-opts)
-               :output (far/freeze #{"D1" "D2"} crypt-opts)})
-
-(far/put-item client-opts
-              :spreadsheets
-              {:customer "hans"
-               :spreadsheet "nase-ring"
-               :name "example.xls"
-               :url "/Users/ltrieloff/Documents/excelsior/resources/helloworld.xlsx"
-               :type "local"
-               :inputs (far/freeze #{"A1", "B1"} crypt-opts)
-               :output (far/freeze #{"D1" "D2"} crypt-opts)})
-
-(far/get-item client-opts :customers {:customer "hans"})
-
-
-(count (far/with-thaw-opts crypt-opts (far/query client-opts
-                                          :spreadsheets {:customer [:eq "hans"]
-                                                         :spreadsheet [:begins-with "nase"]})))
-
-(is-unique-name? "hans" "wurst")
-(is-unique-name? "hans" "nase")
-(count-variants "hans" "nase")
-(make-unique-name "hans" "wurst")
-(make-unique-name "hans" "nase")
 
 (def formula-from-sheet
   (doc/cell-fn "C1"
@@ -87,3 +23,5 @@
 
 ;(spit "target/swagger.json" (slurp (:body (app {:request-method :get, :uri "/swagger.json"}))))
 ;(generate-string (postwalk #(if (and (map? %) (get % "default")) (assoc % 200 {:description "default response"}) %) (parse-string (slurp (:body (app {:request-method :get, :uri "/swagger.json"}))))))
+
+;http://requestb.in/16c4oj21?spreadsheet=/foo/far&A2={{answer_22581205}}&B2={{answer_22581235}}

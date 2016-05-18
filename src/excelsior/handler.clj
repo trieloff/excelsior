52e1dbed6d2c11474588a4c4f2bc8397c0364355
Lars Trieloff <lars@trieloff.net>
2016-05-18 11:36:27 +0200

    Dead code removal society

--- src/excelsior/handler.clj
+++ src/excelsior/handler.clj
@@ -2,63 +2,16 @@
   (:require [compojure.api.sweet :refer :all]
             [ring-aws-lambda-adapter.core :refer [defhandler]]
             [ring.util.http-response :refer :all]
-            [taoensso.faraday :as far]
-            [clojure.string :as str]
             [dk.ative.docjure.spreadsheet :as doc]
-            [ring.swagger.json-schema :as js]
             [excelsior.core :as env]
             [cheshire.core :refer :all]
             [clojure.java.io :as io]
             [schema.core :as s]))
 
-(s/defschema Message {:message String})
-
-(s/defschema Meta {:customer String
-                   :spreadsheet String
-                   :name String
-                   :url String
-                   :type String
-                   :output #{String}
-                   :inputs #{String}})
-
-(s/defschema Spreadsheet {:input {s/Keyword (s/either Long String)}
-                          :meta Meta
-                          :output {s/Keyword (s/either Number String)}})
-
-(def client-opts
-  {;;; For DDB Local just use some random strings here, otherwise include your
-   ;;; production IAM keys:
-   :access-key (env/env :aws-access-key) ;; reading from $AWS_ACCESS_KEY environment variable
-   :secret-key (env/env :aws-secret-key) ;; reading from $AWS_SECRET_KEY environment variable
-
-   ;;; You may optionally override the default endpoint if you'd like to use DDB
-   ;;; Local or a different AWS Region (Ref. http://goo.gl/YmV80o), etc.:
-   :endpoint (env/env :dynamodb-endpoint)                   ; For DDB Local
-   ;; :endpoint "http://dynamodb.eu-west-1.amazonaws.com" ; For EU West 1 AWS region
-   })
-
-(def crypt-opts {:password [:salted (env/env :dynamodb-crypt-key)]})
-
 (defn -init []
   (println "Database Initialization")
 
-  (println "Access key: " (env/env :aws-access-key))
-
-  (println "Checking customers table")
-
-(far/ensure-table client-opts :customers
-                  [:customer :s]
-                  {:throughput {:read 1 :write 1}
-                   :block? true})
-
-  (println "Checking spreadsheets table")
-
-(far/ensure-table client-opts :spreadsheets
-                  [:customer :s]  ; Primary key named "id", (:n => number type)
-                  {:throughput {:read 1 :write 1} ; Read & write capacity (units/sec)
-                   :range-keydef [:spreadsheet :s]
-                   :block? true ; Block thread during table creation
-                     }))
+  (println "Access key: " (env/env :aws-access-key)))
 
 (def init (memoize -init))
 
@@ -73,26 +26,6 @@
                                                               :httpMethod "POST"
                                                               :type "aws" }})
 
-(defn is-unique-name? [customer name]
-  (nil? (far/with-thaw-opts crypt-opts (far/get-item client-opts
-                                             :spreadsheets
-                                             {:customer customer :spreadsheet name}))))
-
-(defn count-variants [customer name]
-  (inc (count (filter #(re-matches (re-pattern (str "^" name "-\\d+")) (:spreadsheet %)) (far/with-thaw-opts crypt-opts (far/query client-opts
-                                          :spreadsheets {:customer [:eq customer]
-                                                         :spreadsheet [:begins-with (str name "-")]}))))))
-
-(defn make-unique-name [customer name]
-  (if (is-unique-name? customer name)
-    name
-    (str name "-" (count-variants customer name))))
-
-
-(defn sheet-formulas [inputs outputs sheet]
-  (into (hash-map) (map #(conj
-                          (vector (keyword %))
-                          (apply doc/cell-fn % (first (doc/sheet-seq (doc/load-workbook sheet))) (sort inputs))) (sort outputs))))
 
 (defn clean-values [value]
   (println value)
@@ -118,85 +51,20 @@
                  (let [body (parse-string (slurp (:body request)) true)
                        answer (-> body :form_response :answers)
                        inputs (filter #(re-matches #"[A-Z]+[0-9]+" %) (-> request :query-params keys))
-                       cellfunc (doc/cell-fn cell (nth (doc/sheet-seq (doc/load-workbook spreadsheet)) sheet) inputs)
+                       cellfunc (apply doc/cell-fn cell (first (doc/sheet-seq (doc/load-workbook spreadsheet))) inputs)
+                       ;cellfunc (doc/cell-fn cell (nth (doc/sheet-seq (doc/load-workbook spreadsheet)) sheet) inputs)
                        fieldids (map #(get (-> request :query-params) %) inputs)
                        values (map clean-values (map (fn [fieldid] (first (filter #(= (-> % :field :id) fieldid) answer))) fieldids))]
-                   (ok {:message spreadsheet
-                        :values values})))
+                   (ok {:spreadsheet spreadsheet
+                        :inputs inputs
+                        :values values
+                        :return (apply cellfunc values)})))
           (GET "/" request
-                :return Message
+                :return s/Any
                 :query-params [spreadsheet :- String]
                 :summary "Calculate response value from Redirect"
                 :swagger aws-gateway-options
                 (ok {:message spreadsheet})))
-  (context "/formula" []
-            (GET "/:customer/:spreadsheet" request
-                  :return Spreadsheet
-                  :path-params [customer :- String spreadsheet :- String]
-                  :summary "calculate the response value"
-                  :swagger aws-gateway-options
-                  (ok (let
-                        [db (init)
-                         meta (far/with-thaw-opts crypt-opts (far/get-item client-opts
-                                            :spreadsheets {:customer customer
-                                                           :spreadsheet spreadsheet}))
-                         inputs   (:inputs meta)
-                         outputs  (:output meta)
-                         params   (:params request)
-                         sheet    (:url meta)
-                         fnmap    (sheet-formulas inputs outputs sheet)
-                         values   (vals (into (sorted-map) params))]
-                        {:input (:params request)
-                       :meta meta
-                       :output (zipmap (keys fnmap) (map #(apply % values) (vals fnmap)))})))
-
-           (POST "/:customer/:spreadsheet" []
-                   :return Meta
-                   :path-params [customer :- String spreadsheet :- String]
-                   :body-params [inputs :- (js/field [String] {:collectionFormat "multi" :description "the cells that will be used as input. Use cell references such as A1"})
-                                 outputs :- (js/field [String] {:collectionFormat "multi" :description "the cells that will be used as output. Use cell references such as A2"})]
-                   :summary "Update input and output cells for a spreadsheet"
-                   :swagger aws-gateway-options
-                   (ok (let [db (init)
-                             meta (far/with-thaw-opts crypt-opts (far/get-item client-opts
-                                            :spreadsheets {:customer customer
-                                                           :spreadsheet spreadsheet}))
-                             new-plain (assoc meta :inputs (set inputs) :output (set outputs))
-                             new-crypt (assoc meta
-                                         :inputs (far/freeze (set inputs) crypt-opts)
-                                         :output (far/freeze (set outputs) crypt-opts))
-                             put (far/put-item client-opts
-                                               :spreadsheets
-                                               new-crypt)]
-                         new-plain)))
-
-           (POST "/:customer/" []
-                   :return Meta
-                   :path-params [customer :- String]
-                   :body-params [inputs :- (js/field [String] {:collectionFormat "multi" :description "the cells that will be used as input. Use cell references such as A1"})
-                                 outputs :- (js/field [String] {:collectionFormat "multi" :description "the cells that will be used as output. Use cell references such as A2"})
-                                 name :- String]
-                   :summary "Create a new spreadsheet"
-                   :swagger aws-gateway-options
-                   (ok (let [db (init)
-                             meta {:customer     customer
-                                   :spreadsheet  (make-unique-name customer name)
-                                   :name         name
-                                   :url          "/Users/ltrieloff/Documents/excelsior/resources/helloworld.xlsx"
-                                   :type         "local"
-                                   :inputs       (far/freeze (set inputs) crypt-opts)
-                                   :output       (far/freeze (set outputs) crypt-opts)}
-                             put (far/put-item client-opts
-                                               :spreadsheets
-                                               meta)]
-                         (assoc meta :inputs (set inputs) :output (set outputs)))))
-            (GET "/:customer" []
-                  :return [String]
-                  :path-params [customer :- String]
-                  :summary "List all spreadsheets for a customer"
-                  :swagger aws-gateway-options
-                  (ok (map #(:spreadsheet %) (far/with-thaw-opts crypt-opts (far/query client-opts
-                                                                :spreadsheets {:customer [:eq customer]}))))))
   (context "/hello" []
     :tags ["hello"]
     (GET "/" []
